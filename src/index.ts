1fb1a6d6700660c6b3c4327c3c6890ca553d5262
nils865 <nils865@users.noreply.github.com>
2023-07-27 18:13:31 +0000

    format files

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import chalk from 'chalk';
-import { display_artist_scoreboard, get_id, run_analysis, select_stats } from './cli.js';
+import {
+	display_artist_scoreboard,
+	get_id,
+	run_analysis,
+	select_stats,
+} from './cli.js';
 
-console.log(`Welcome to ${chalk.green("Your favourite Spotify Analytics CLI")}`)
+console.log(
+	`Welcome to ${chalk.green('Your favourite Spotify Analytics CLI')}`,
+);
 
-const scope = await select_stats()
+const scope = await select_stats();
 
-const id = await get_id(scope)
+const id = await get_id(scope);
 
-const data = await run_analysis(scope, id)
+const data = await run_analysis(scope, id);
 
-display_artist_scoreboard(data)
+display_artist_scoreboard(data);
