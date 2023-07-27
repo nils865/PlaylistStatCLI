afb5d78a8f691bf6d536fd726cd9c025cf9645cf
Nils <nils.staas0806@gmail.com>
2023-07-27 19:46:35 +0200

    Removed Old debug code

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,5 @@
-import { artist_counter, sort_object } from './analysis/dataHandling.js';
-import { get_access_token } from './spotifyAPI.js';
-import { get_all_user_songs, get_user_playlists } from './analysis/userData.js';
 import chalk from 'chalk';
 import { get_id, run_analysis, select_stats } from './cli.js';
-
-// const token = await get_access_token();
-
-// const playlistID = '4NAeFwwinX6tS5RN5voNbg';
-// const user_playlists = await get_playlist_content(playlistID, token);
-
-// const artist_count = await artist_counter(user_playlists)
-
-// const userID = '31vcslluzy32h77ak63kmdq4uqgq';
-// const userPlaylists = await get_user_playlists(userID, token);
-// const all_songs = await get_all_user_songs(userPlaylists, token);
-// const artist_count = await artist_counter(all_songs);
-
-// console.log(sort_object(artist_count));
-
-// console.log(chalk.bgGreen("test"))
 
 console.log(`Welcome to ${chalk.green("Your favourite Spotify Analytics CLI")}`)
 
