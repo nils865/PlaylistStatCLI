313c51ba77d95848dff13b33f53cc4fb596a56e4
Nils <nils.staas0806@gmail.com>
2023-07-27 18:11:30 +0200

    Modularized Data Handling and added better typing

---
+++ src/analysis/playlistaData.ts
@@ -0,0 +1,27 @@
+import { Song } from "./dataHandling.js";
+import { spotify_get_request } from "../spotifyAPI.js";
+
+export async function get_playlist_content(playlistID: string, token: string) {
+    const data = await spotify_get_request(`/playlists/${playlistID}/tracks`, token)
+
+    const playlist: Song[] = []
+
+    for (let i = 0; i < data.items.length; i++) {
+		const song = data.items[i].track;
+
+		if (song == null) continue;
+
+		const current_song: Song = {
+			title: song.name,
+			artists: [],
+		};
+
+		for (let j = 0; j < song.artists.length; j++) {
+			current_song.artists.push(song.artists[j].name);
+		}
+
+		playlist.push(current_song);
+	}
+
+    return playlist
+}
--- src/index.ts
+++ src/index.ts
@@ -1,103 +1,6 @@
-import { get_access_token, spotify_get_request } from './spotifyAPI.js';
-
-const sort_object = (obj: { [key: string]: number }) => {
-	const keyValueArray = Object.entries(obj);
-
-	keyValueArray.sort((a, b) => b[1] - a[1]);
-
-	return Object.fromEntries(keyValueArray);
-};
-
-const get_playlist_content = async (playlistID, token) => {
-	const data = await spotify_get_request(`/playlists/${playlistID}/tracks`, token);
-
-	const playlist = [];
-
-	for (let i = 0; i < data.items.length; i++) {
-		const song = data.items[i].track;
-
-		if (song == null) continue;
-
-		const current_song = {
-			title: song.name,
-			artists: [],
-		};
-
-		for (let j = 0; j < song.artists.length; j++) {
-			current_song.artists.push(song.artists[j].name);
-		}
-
-		playlist.push(current_song);
-	}
-
-	return playlist;
-};
-
-const artist_counter = async playlist => {
-	const artists = {};
-	const finished_songs = [];
-
-	for (let i = 0; i < playlist.length; i++) {
-		const song = playlist[i];
-
-		let skip = false;
-		finished_songs.forEach(current_song => {
-			if (current_song.title === song.title) {
-				let next = false;
-
-				for (let j = 0; j < current_song.artists.length; j++) {
-					if (current_song.artists[j] !== song.artists[j]) {
-						next = true;
-						break;
-					}
-				}
-
-				if (!next) {
-					skip = true;
-				}
-			}
-		});
-
-		if (skip) continue;
-
-		song.artists.forEach(artist => {
-			if (artist in artists) artists[artist]++;
-			else artists[artist] = 1;
-		});
-
-		finished_songs.push(song);
-	}
-
-	return artists;
-};
-
-const get_user_playlists = async (userID, token) => {
-	const playlists = [];
-
-	const data = (await spotify_get_request(`/users/${userID}/playlists`, token)).items;
-
-	data.forEach(playlist => {
-		playlists.push(playlist.id);
-	});
-
-	return playlists;
-};
-
-const get_all_user_songs = async (playlists, token) => {
-	const all_songs = [];
-
-	for (let i = 0; i < playlists.length; i++) {
-		const id = playlists[i];
-
-		const content = await get_playlist_content(id, token);
-
-		content.forEach(song => {
-			all_songs.push(song);
-		});
-	}
-
-	return all_songs;
-};
+import { artist_counter, sort_object } from "./analysis/dataHandling.js";
+import { get_access_token } from "./spotifyAPI.js";
+import { get_all_user_songs, get_user_playlists } from "./analysis/userData.js";
 
 const token = await get_access_token();
 
@@ -105,7 +8,6 @@ const token = await get_access_token();
 // const user_playlists = await get_playlist_content(playlistID, token);
 
 // const artist_count = await artist_counter(user_playlists)
-// console.log(artist_count);
 
 const userID = '31vcslluzy32h77ak63kmdq4uqgq';
 const userPlaylists = await get_user_playlists(userID, token);
