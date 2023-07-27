d5bc547f4257963dd6d8581784d2dbc065bca20d
Nils <nils.staas0806@gmail.com>
2023-07-27 19:50:40 +0200

    Added loading icon for fetching data

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,7 @@ import { get_access_token } from './spotifyAPI.js'
 import { get_playlist_content } from './analysis/playlistaData.js'
 import { Song, artist_counter } from './analysis/dataHandling.js'
 import { get_all_user_songs, get_user_playlists } from './analysis/userData.js'
+import { createSpinner } from 'nanospinner'
 
 export type StatType = "User" | "Playlist"
 
@@ -34,14 +35,24 @@ export async function run_analysis(scope: StatType, id: string) {
     const token = await get_access_token();
     let songList: Song[]
 
-    if (scope === 'Playlist') {
-        songList = await get_playlist_content(id, token);
-    } else if (scope === 'User') {
-        const userPlaylists = await get_user_playlists(id, token);
-        songList = await get_all_user_songs(userPlaylists, token);
-    } else return
+    const spinner = createSpinner('Get Data from spotify').start()
+    let artist_count
 
-    const artist_count = await artist_counter(songList)
+    try {
+        if (scope === 'Playlist') {
+            songList = await get_playlist_content(id, token);
+        } else if (scope === 'User') {
+            const userPlaylists = await get_user_playlists(id, token);
+            songList = await get_all_user_songs(userPlaylists, token);
+        } else return
+
+        artist_count = await artist_counter(songList)
+
+        spinner.success()
+    } catch (error) {
+        spinner.error({"text": "Couldn't get Data"})
+        process.exit(1)
+    }
 
     return artist_count
 }
